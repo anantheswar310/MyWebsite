1008c440905ad60a849ce408aad84ae9ced7ac75
Ananth <anantheswar.310@gmail.com>
2024-06-30 16:21:13 +0300

    Initial commit

---
+++ src/data/education.js
@@ -0,0 +1,20 @@
+export const education = [
+  {
+    eduDate: "2009",
+    eduTitle: "Batchelor of Technology with Honors",
+    eduIns: "Indian Institute of Information Technology",
+    eduDesc: "8.75 CGPA",
+  },
+  {
+    eduDate: "2004",
+    eduTitle: "Higher Secondary",
+    eduIns: "Ratnam Junior College",
+    eduDesc: "93.6%",
+  },
+  {
+    eduDate: "2002",
+    eduTitle: "Secondary",
+    eduIns: "Kurnool Public School",
+    eduDesc: "90%",
+  }
+];
