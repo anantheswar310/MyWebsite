e6fce0aaf8bb9c3202718333084e50e73cf68e29
Ananth <anantheswar.310@gmail.com>
2024-07-21 14:06:29 +0300

    updated blog posts

--- src/components/BlogPosts/BlogPost.jsx
+++ src/components/BlogPosts/BlogPost.jsx
@@ -16,45 +16,48 @@ const BlogPost = (props) => {
         <SectionTitle
           title="Featured"
           titleInner="Blog Posts"
-          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore."
+          //desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore."
         ></SectionTitle>
         <div className="postsList grid gap-[1.875rem] lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 grid-cols-1 relative w-full">
           {posts.map(
             (item, index) =>
-              index < 3 && (
+              index < 2 && (
                 <div className="postBox group relative w-full" key={index}>
                   <div className="postImg overflow-hidden rounded-[10px] sm:rounded-[1.25rem] md:rounded-[1.875rem] w-full">
-                    <Link to={`/blog/${item.id}`} title={item.postTitle}>
+                    {/* <Link to={`/blog/${item.id}`} title={item.postTitle}> */}
+
+                    <a href={item.postLink} target="_blank" >
+                    
                       <img
                         className="w-full group-hover:scale-105"
                         src={item.postImg}
                         alt={item.postTitle}
                       ></img>
-                    </Link>
+                    </a>
                   </div>
                   <div className="postInfo relative z-[1] px-5 sm:px-8 md:px-10 py-5 sm:py-7 md:py-8 mt-[-4.6875rem] inset-x-[10px] max-w-[calc(100%-1.25rem)]">
                     <div
                       className="before:absolute before:inset-0 before:bg-gray before:rounded-[10px] before:sm:rounded-[1.25rem] before:md:rounded-[1.875rem] before:opacity-70 before:z-[1] bg-blend-multiply absolute rounded-[10px] sm:rounded-[1.25rem] md:rounded-[1.875rem] inset-0 w-full bg-no-repeat bg-center bg-cover z-[-1] bg-gray"
                       style={{ backgroundImage: `url(${PatternImg2})` }}
                     ></div>
-                    <span className="flex items-center text-accent gap-[10px] text-[.9375rem] font-NunitoSans font-semibold">
+                    {/* <span className="flex items-center text-accent gap-[10px] text-[.9375rem] font-NunitoSans font-semibold">
                       <FaCalendarAlt></FaCalendarAlt>{" "}
                       <Link className="hover:text-accent2" to={item.postDateLink} title={item.postDate}>
                         {item.postDate}
                       </Link>
-                    </span>
+                    </span> */}
                     <h3 className="text-accent2 text-[1.25rem] font-Poppins font-semibold mt-2">
-                      <Link className="hover:text-accent" to={`/blog/${item.id}`} title={item.postTitle}>
+                      <a href={item.postLink} className="hover:text-accent" title={item.postTitle} target="_blank" >
                         {item.postTitle}
-                      </Link>
+                      </a>
                     </h3>
                     <i className="bg-[#d5daf2] h-[1px] border-0 relative before:absolute before:bg-accent before:w-[3.125rem] before:h-[3px] before:z-[1] block before:mt-[-1px] mt-5 mb-4"></i>
-                    <span className="flex items-center text-accent gap-1 text-[1rem] font-NunitoSans font-bold">
+                    {/* <span className="flex items-center text-accent gap-1 text-[1rem] font-NunitoSans font-bold">
                       By
                       <Link className="hover:text-accent2" to={item.postAuthorLink} title={item.postAuthor}>
                         {item.postAuthor}
                       </Link>
-                    </span>
+                    </span> */}
                   </div>
                   {/* Post Box */}
                 </div>
@@ -62,7 +65,7 @@ const BlogPost = (props) => {
           )}
           {/* Posts List */}
         </div>
-        <div className="viewAll text-center w-full mt-[3.125rem] md:mt-[4.0625rem]">
+        {/* <div className="viewAll text-center w-full mt-[3.125rem] md:mt-[4.0625rem]">
           <p className="text-desc2 font-NunitoSans text-[1rem] md:text-[1.125rem]">
             Want to see my professional work.{" "}
             <Link
@@ -73,8 +76,9 @@ const BlogPost = (props) => {
               Click here to View More
             </Link>
           </p>
-          {/* View All */}
-        </div>
+          
+        </div> */}
+        {/* View All */}
       </div>
     </section>
   );
--- src/data/posts.js
+++ src/data/posts.js
@@ -1,5 +1,5 @@
-import PostImg1 from "../assets/images/resources/postImg1-1.jpg";
-import PostImg2 from "../assets/images/resources/postImg1-2.jpg";
+import PostImg1 from "../assets/images/bsod.jpeg";
+import PostImg2 from "../assets/images/gpterror.png";
 import PostImg3 from "../assets/images/resources/postImg1-3.jpg";
 import PostImg4 from "../assets/images/resources/postImg1-4.jpg";
 import PostImg5 from "../assets/images/resources/postImg1-5.jpg";
@@ -9,10 +9,10 @@ export const posts = [
   {
     id: 1,
     postImg: PostImg1,
-    postDate: "January 21, 2022",
+    postDate: "January 21, 2024",
     postDateLink: "#",
-    postTitle: "Spring Which I Enjoy With For Effective Agile Team",
-    postLink: "#",
+    postTitle: "Global Outage Apocalypse: How to Avoid it in the Future",
+    postLink: "https://medium.com/@anantheswar310/global-outage-apocalypse-how-to-avoid-it-in-the-future-f9086b9118be",
     postAuthor: "Berline Tokyo",
     postAuthorLink: "#",
   },
@@ -21,8 +21,8 @@ export const posts = [
     postImg: PostImg2,
     postDate: "February 18, 2022",
     postDateLink: "#",
-    postTitle: "But some of the current projects that make",
-    postLink: "#",
+    postTitle: "Did Chat GPT get a simple addition wrong twice?",
+    postLink: "https://medium.com/@anantheswar310/did-chat-gpt-get-a-simple-addition-wrong-twice-1fcbbc96ed22",
     postAuthor: "John Doe",
     postAuthorLink: "#",
   },
